75fad3d349e8b89af30b1dedf6ad38bf97df8931
meowmad-ali <aaarjavk@gmail.com>
2025-07-10 17:48:35 +0530

    Add files via upload

---
+++ firmware/keymaps/config.h
@@ -0,0 +1,14 @@
+
+
+#pragma once
+
+
+/* key matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 16
+#define MATRIX_MUX_COLS 4
+
+#define MATRIX_ROW_PINS { B6, B2, B3, B1, F7 }
+#define MATRIX_COL_PINS { }
+#define MATRIX_COL_MUX_PINS { D7, E6, B4, B5 }
+
